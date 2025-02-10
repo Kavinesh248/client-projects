80383a51a505d264a0bb706a0d6f054d911b4c3d
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-10 12:54:39 +0530

    pages

--- verbalvista/src/pages/Home.jsx
+++ verbalvista/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../features/Header/Header";
 import Hero from "../features/Hero/Hero";
+import WhyChooseUs from "../features/WhyChooseUs/WhyChooseUs";
 
 const Home = function () {
   return (
     <main>
       <Header />
       <Hero />
+      <WhyChooseUs />
     </main>
   );
 };
