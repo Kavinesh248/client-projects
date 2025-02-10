7b607af5fe1befc646e47e3823e8eae95f20818f
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-10 12:54:27 +0530

    asset folder

--- verbalvista/src/assets/assets.js
+++ verbalvista/src/assets/assets.js
@@ -4,6 +4,12 @@ import heroImage from "./hero-banner.png";
 import brandLogo from "./logo.png";
 import heartImage from "./heart.png";
 import circleImage from "./circle.png";
+import client1 from "./customer-1.jpg";
+import client2 from "./customer-2.jpg";
+import client3 from "./customer-3.jpg";
+import client4 from "./customer-4.jpg";
+import client5 from "./customer-5.jpg";
+import client6 from "./customer-6.jpg";
 
 export const assets = {
   hamburger,
@@ -12,4 +18,10 @@ export const assets = {
   brandLogo,
   heartImage,
   circleImage,
+  client1,
+  client2,
+  client3,
+  client4,
+  client5,
+  client6,
 };
