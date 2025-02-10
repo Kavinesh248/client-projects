ecd1e8083a6459d2766e46185192156bcda72d04
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-10 12:53:33 +0530

    hero section

--- verbalvista/src/features/Hero/Hero.jsx
+++ verbalvista/src/features/Hero/Hero.jsx
@@ -15,8 +15,24 @@ const Hero = function () {
             predefined chunks as necessary,
           </p>
 
-          <button className={styles.btn}>Start the Journey</button>
-          <button className={styles.btn}>Find Courses &rarr;</button>
+          <button className={styles.btn}>Start Now</button>
+          <button className={`${styles.btn} ${styles.btnWhite}`}>
+            Find Courses &rarr;
+          </button>
+
+          <aside className={styles.deliverdReview}>
+            <div className={styles.reviewImgs}>
+              <img src={assets.client1} alt="client image" />
+              <img src={assets.client2} alt="client image" />
+              <img src={assets.client3} alt="client image" />
+              <img src={assets.client4} alt="client image" />
+              <img src={assets.client5} alt="client image" />
+              <img src={assets.client6} alt="client image" />
+            </div>
+            <p className={styles.reviewText}>
+              Join our community to <br /> extend your knowledge!
+            </p>
+          </aside>
         </div>
         <div className={styles.heroImageBox}>
           <img src={assets.heroImage} alt="hero image" />
