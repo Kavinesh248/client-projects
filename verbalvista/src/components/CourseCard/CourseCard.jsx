66aeff97e518f0c6eb8714c89ba9e4fe5b3682a1
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-14 12:00:50 +0530

    course card separate comp

---
+++ verbalvista/src/components/CourseCard/CourseCard.jsx
@@ -0,0 +1,131 @@
+import styles from "./CourseCard.module.css";
+
+const CourseCard = function ({
+  heroImage,
+  nameBg,
+  nameColor,
+  courseName,
+  courseTitle,
+  coursePrice,
+}) {
+  console.log(heroImage);
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.imgContainer}>
+        <img
+          className={styles.heroImage}
+          src={heroImage}
+          alt="Spinning glass cube"
+        />
+      </div>
+      <main className={styles.mainContent}>
+        <div className={styles.courseContentWrapper}>
+          <div className={styles.courseRatingWrapper}>
+            <div className={styles.ratingStar}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <span>4.6</span>
+            <span>(12)</span>
+          </div>
+          <p
+            className={styles.courseName}
+            style={{ backgroundColor: nameBg, color: nameColor }}
+          >
+            spoken {courseName}
+          </p>
+        </div>
+        <p className={styles.courseTitle}>{courseTitle}</p>
+        <div className={styles.readMore}>More details &rarr;</div>
+      </main>
+      <div className={styles.courseDurationBox}>
+        <div className={styles.courseDuration}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={styles.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <span>3 mnths</span>
+        </div>
+        <div className={styles.courseMode}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={styles.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819"
+            />
+          </svg>
+          <span>Offline/Online</span>
+        </div>
+        <div className={styles.courseLevel}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={styles.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+            />
+          </svg>
+          <span>Beginner</span>
+        </div>
+      </div>
+      <div className={styles.coursePriceBox}>
+        <div className={styles.priceBox}>
+          <span>₹4,000</span>
+          <span>₹{coursePrice}/m</span>
+        </div>
+        <div className={styles.enroll}>
+          <span>Enroll</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={styles.icon}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseCard;
