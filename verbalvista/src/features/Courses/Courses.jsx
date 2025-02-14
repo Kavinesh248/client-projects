1f6dd9ee2a3aa3439df1f640c2e439a0a79ce2e1
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-14 12:00:28 +0530

    courses section completed

--- verbalvista/src/features/Courses/Courses.jsx
+++ verbalvista/src/features/Courses/Courses.jsx
@@ -1,53 +1,36 @@
 import { assets } from "../../assets/assets";
+import CourseCard from "../../components/CourseCard/CourseCard";
 import styles from "./Courses.module.css";
 
 const Courses = function () {
+  console.log(assets.spokenEnglish);
   return (
     <section className={`${styles.sectionCourses} container`}>
-      <div className={styles.cardContainer}>
-        <img
-          className={styles.heroImage}
-          src={assets.course1}
-          alt="Spinning glass cube"
+      <article className={styles.courseHeader}>
+        <h1>Popular Courses</h1>
+        <p>
+          A curated collection of amazing courses, chosen based on popularity
+          and ratings by students.
+        </p>
+      </article>
+      <div className={styles.courseContainer}>
+        <CourseCard
+          heroImage={assets.spokenEnglish}
+          nameBg="#fef1e6"
+          nameColor="#f57005"
+          courseName="english"
+          courseTitle="Master Fluent English: Speak with Confidence."
+          coursePrice="2,500"
         />
-        <main className={styles.mainContent}>
-          <div className={styles.courseContentWrapper}>
-            <div className={styles.courseRatingWrapper}>
-              <div className={styles.ratingStar}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <span>4.3</span>
-              <span>(31)</span>
-            </div>
 
-            <p className={styles.courseName}>Spoken English</p>
-          </div>
-          <p className={styles.courseTitle}>Learn Spoken English.</p>
-        </main>
-        {/* <div className={styles.cardAttribute}>
-          <img
-            src="https://i.postimg.cc/SQBzNQf1/image-avatar.png"
-            alt="avatar"
-            className={styles.smallAvatar}
-          />
-          <p>
-            Creation of
-            <span className={styles.spanEl}>
-              <a href="#">Jules Wyvern</a>
-            </span>
-          </p>
-        </div> */}
+        <CourseCard
+          heroImage={assets.spokenFrench}
+          nameBg="#ffeeee"
+          nameColor="#e03131"
+          courseName="french"
+          courseTitle="Speak French Like a Native: Fast & Easy"
+          coursePrice="2,500"
+        />
       </div>
     </section>
   );
