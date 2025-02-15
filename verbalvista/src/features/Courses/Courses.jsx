52e84a28aa31401902e2d05202af39318388c13b
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-15 12:18:47 +0530

    courses section updated

--- verbalvista/src/features/Courses/Courses.jsx
+++ verbalvista/src/features/Courses/Courses.jsx
@@ -1,5 +1,6 @@
 import { assets } from "../../assets/assets";
 import CourseCard from "../../components/CourseCard/CourseCard";
+import { courses } from "../../utils/courseDetails";
 import styles from "./Courses.module.css";
 
 const Courses = function () {
@@ -14,23 +15,9 @@ const Courses = function () {
         </p>
       </article>
       <div className={styles.courseContainer}>
-        <CourseCard
-          heroImage={assets.spokenEnglish}
-          nameBg="#fef1e6"
-          nameColor="#f57005"
-          courseName="english"
-          courseTitle="Master Fluent English: Speak with Confidence."
-          coursePrice="2,500"
-        />
-
-        <CourseCard
-          heroImage={assets.spokenFrench}
-          nameBg="#ffeeee"
-          nameColor="#e03131"
-          courseName="french"
-          courseTitle="Speak French Like a Native: Fast & Easy"
-          coursePrice="2,500"
-        />
+        {courses.map((course, index) => (
+          <CourseCard key={index} {...course} />
+        ))}
       </div>
     </section>
   );
