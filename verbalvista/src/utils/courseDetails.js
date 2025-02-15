56a4d9d480c8f42e9e468f901fef93f659c4100d
kavinesh <kavineshkavinesh860@gmail.com>
2025-02-15 12:19:34 +0530

    utilities folder

---
+++ verbalvista/src/utils/courseDetails.js
@@ -0,0 +1,62 @@
+import { assets } from "../assets/assets";
+
+export const courses = [
+  {
+    heroImage: assets.spokenEnglish,
+    nameBg: "#fef1e6",
+    nameColor: "#f57005",
+    courseName: "spoken english",
+    courseTitle: "Master Fluent English: Speak with Confidence.",
+    courseOffPrice: "5,500",
+    coursePrice: "2,500",
+    rating: {
+      ratingAverage: 4.5,
+      noOfPeopleRated: 12,
+    },
+    duration: 3,
+  },
+  {
+    heroImage: assets.spokenFrench,
+    nameBg: "#ffeeee",
+    nameColor: "#e03131",
+    courseName: "french tuition",
+    courseTitle: "Speak French Confidently with Our Tuition Program.",
+    courseOffPrice: "4,000",
+    coursePrice: "2,500",
+    rating: {
+      ratingAverage: 4.3,
+      noOfPeopleRated: 7,
+    },
+    duration: 3,
+  },
+  {
+    heroImage: assets.ielts,
+    nameBg: "#f0ecf9",
+    nameColor: "#6941C6",
+    courseName: "ielts",
+    courseTitle: "Crack IELTS with Confidence: Learn & Succeed.",
+    courseOffPrice: "25,000",
+    coursePrice: "15,000",
+    isBlur: true,
+    rating: {
+      ratingAverage: 4.7,
+      noOfPeopleRated: 3,
+    },
+    duration: 3,
+  },
+  {
+    heroImage: assets.phonetics,
+    nameBg: "#e6f6ff",
+    nameColor: "#00A3FF",
+    courseName: "phonetics",
+    courseTitle: "Perfect Pronunciation: A Phonetics Guide.",
+    courseOffPrice: "15,000",
+    coursePrice: "8,000",
+    isBlur: true,
+    rating: {
+      ratingAverage: 4.3,
+      noOfPeopleRated: 4,
+    },
+    duration: 4,
+  },
+];
